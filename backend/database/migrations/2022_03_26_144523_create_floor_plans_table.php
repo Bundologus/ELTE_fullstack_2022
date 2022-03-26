abc646458ce6612cc24a4b6ab5fa4accc7bd7c77
Perlaki András <cbsdzh@inf.elte.hu>
2022-03-26 15:47:50 +0100

    add floor_plans table migration

---
+++ backend/database/migrations/2022_03_26_144523_create_floor_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFloorPlansTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('floor_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('unit_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('width');
+            $table->integer('height');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('floor_plans');
+    }
+}
