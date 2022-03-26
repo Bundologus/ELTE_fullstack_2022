1458646a5dfce76e2ddcd4a7ac9eb1c01ea6228d
Perlaki András <cbsdzh@inf.elte.hu>
2022-03-26 15:49:19 +0100

    add reservables table migration

---
+++ backend/database/migrations/2022_03_26_144803_create_reservables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservablesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('reservables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('min_spaces');
+            $table->integer('max_spaces');
+            $table->time('min_time');
+            $table->time('max_time');
+            $table->time('time_step');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('reservables');
+    }
+}
