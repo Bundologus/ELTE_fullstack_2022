0cfed53a2477b133a0267cf28afc0fa8f4907da2
Perlaki András <cbsdzh@inf.elte.hu>
2022-03-30 17:02:01 +0200

    add opening hours model

---
+++ backend/app/Models/OpeningHours.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OpeningHours extends Model {
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'unit_id',
+        'reservable_id',
+        'day_from',
+        'day_to',
+        'specific_date_from',
+        'specific_data_to',
+        'time_from',
+        'time_to',
+    ];
+}
