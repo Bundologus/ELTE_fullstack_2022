17dcc267987c2cb10f96cddbada7abc0a27c48ee
Perlaki András <cbsdzh@inf.elte.hu>
2022-05-14 19:28:39 +0200

    add opneing hours seeding

--- backend/database/seeders/UnitSeeder.php
+++ backend/database/seeders/UnitSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\FloorPlan;
 use App\Models\FpEntity;
+use App\Models\OpeningHours;
 use App\Models\Reservable;
 use App\Models\Unit;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,7 @@ class UnitSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        $units = Unit::factory(10)->has(FloorPlan::factory(1))->create();
+        $units = Unit::factory(10)->has(FloorPlan::factory(1))->has(OpeningHours::factory())->create();
 
         foreach ($units as $unit) {
             $floorPlan = $unit->floorPlan;
@@ -29,6 +30,10 @@ class UnitSeeder extends Seeder {
 
             $reservable_with_time = Reservable::factory()->withTime()->create();
             FpEntity::factory()->for($floorPlan)->for($reservable_with_time)->create();
+
+            $special_hours_count = rand(3, 7);
+            OpeningHours::factory($special_hours_count)->specificDate()->for($unit)->create();
+            OpeningHours::factory(5)->specificDate()->singleDay()->for($unit)->create();
         }
     }
 }
