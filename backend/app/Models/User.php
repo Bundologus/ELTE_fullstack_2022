ba09ba463969634a085684bcb15de90c011e093e
Perlaki András <cbsdzh@inf.elte.hu>
2022-05-14 19:06:35 +0200

    add 'admin' attribute

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable {
         'email',
         'phone',
         'password',
+        'admin',
     ];
 
     /**
@@ -42,10 +43,6 @@ class User extends Authenticatable {
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function roles() {
-        return $this->belongsToMany(Role::class)->withTimestamps();
-    }
 
     public function reservations() {
         return $this->hasMany(Reservation::class);
