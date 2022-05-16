9d4f44f89f3d284f6704e32ae7b96aa463096977
Perlaki András <cbsdzh@inf.elte.hu>
2022-05-16 06:35:59 +0200

    add address resources

--- backend/app/Http/Resources/DistrictCollection.php
+++ backend/app/Http/Resources/DistrictCollection.php
@@ -4,16 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class Country extends ResourceCollection
-{
+class DistrictCollection extends ResourceCollection {
     /**
      * Transform the resource collection into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
+    public function toArray($request) {
+        return [
+            'city_id' => $this->city_id,
+            'id' => $this->id,
+            'name' => $this->city->name . ", " . $this->name,
+            'post_code' => $this->post_code,
+        ];
     }
 }
