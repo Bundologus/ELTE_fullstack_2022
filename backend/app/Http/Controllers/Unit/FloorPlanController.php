3d875fa52d26549a35fa7537e575c5d20bcc6e8c
Perlaki András <cbsdzh@inf.elte.hu>
2022-05-20 08:02:20 +0200

    add FloorPlanController logic

--- backend/app/Http/Controllers/Unit/FloorPlanController.php
+++ backend/app/Http/Controllers/Unit/FloorPlanController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Unit;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\FloorPlanResource;
+use App\Models\FloorPlan;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class FloorPlanController extends Controller {
@@ -13,7 +16,22 @@ class FloorPlanController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request, $unit_id) {
-        //
+        $data = $request->all();
+        $unit = Unit::find($unit_id);
+
+        if ($unit == null) {
+            return abort(404, 'Unit resource not found.');
+        } else if ($unit->floor_plan != null) {
+            return abort(409, 'Conflict. Unit already has a floor plan.');
+        }
+
+        $floorPlan = FloorPlan::create([
+            'unit_id' => $unit_id,
+            'width' => $data['width'],
+            'height' => $data['height'],
+        ]);
+
+        return new FloorPlanResource($floorPlan);
     }
 
     /**
@@ -23,7 +41,13 @@ class FloorPlanController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function show($unit_id) {
-        //
+        $floorPlan = FloorPlan::where('unit_id', '=', $unit_id)->getFirst();
+
+        if ($floorPlan == null) {
+            return abort(404, 'Resource not found.');
+        }
+
+        return new FloorPlanResource($floorPlan);
     }
 
     /**
@@ -34,7 +58,19 @@ class FloorPlanController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $unit_id) {
-        //
+        $data = $request->all();
+        $floorPlan = FloorPlan::where('unit_id', '=', $unit_id)->getFirst();
+
+        if ($floorPlan == null) {
+            return abort(404, 'Resource not found.');
+        }
+
+        foreach ($data as $key => $value) {
+            $floorPlan[$key] = $value;
+        }
+        $floorPlan->save();
+
+        return new FloorPlanResource($floorPlan);
     }
 
     /**
@@ -44,6 +80,14 @@ class FloorPlanController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function destroy($unit_id) {
-        //
+        $floorPlan = FloorPlan::where('unit_id', '=', $unit_id)->getFirst();
+
+        if ($floorPlan == null) {
+            return abort(404, 'Resource not found.');
+        }
+
+        $floorPlan->delete();
+
+        return response('Success.', 200);
     }
 }
