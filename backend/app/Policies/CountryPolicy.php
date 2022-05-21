07fce8978385feb7badff3e9c1bb278ab5eb8c86
Perlaki András <cbsdzh@inf.elte.hu>
2022-05-21 21:41:38 +0200

    add address policies

---
+++ backend/app/Policies/CountryPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class CountryPolicy {
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function read(User $user) {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create/modify/delete models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function write(User $user) {
+        return $user->admin
+            ? Response::allow()
+            : Response::deny('You must be an administrator.');
+    }
+}
